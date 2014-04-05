540f91e2b34cd4d83d5a83309f39f2a9a3e5cb38
Maarten Deville <maarten.deville@gmail.com>
2014-04-05 17:04:56 +0200

    Added gracenote integration

--- gracenote.js
+++ gracenote.js
@@ -1,13 +1,37 @@
-function getGraceNoteTracks( graceNoteMoodId ) {
+function getGraceNoteTracks (graceNoteMoodId) {
+	function fetchDeezerID(album){
+		var deezid = 0;
+
+		deezid = album['TRACK'][0]['XID'][0]['VALUE']
+		return 	deezid;
+	}
+	var moodparam = "?mood_code="
+	var idString = graceNoteMoodId.toString()
+	var tempurl = "http://mhdapi-640468004.eu-west-1.elb.amazonaws.com/users/123/playlist"
 	
-	/* // do a get request
-	$.ajax({
-	    url: 'http://url/path/',
-	    type: 'GET',
-	    data: 'param1=value1&param2=value',
-	    success: function() { alert('GET completed'); }
-	});
-	*/
-	
-	return [60978718,18232696,60978718];
-}
+	moodparam = moodparam.concat(idString)
+	tempurl = tempurl.concat(moodparam)
+
+     jQuery.ajax({
+         type: "GET",
+         url: tempurl,
+         contentType: "application/json; charset=utf-8",
+         dataType: "json",
+         success: function (data) {
+         	var albumlist =data['RESPONSE'][0].ALBUM
+         	var deezidlist = new Array();
+         	for(var i = 0;i<albumlist.length;i++){
+         		deezidlist.push(fetchDeezerID(albumlist[i]))
+         	}
+         	return(deezidlist)
+             // do something
+             //alert(data.length)
+             //alert(data.response[0].status)
+
+         },
+
+         error: function (jqXHR, status) {
+             // error handler
+         }
+	})
+};
