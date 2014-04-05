1ec0fc27433f327046346db947111cee1effd8de
Anthony Liekens <anthony@liekens.net>
2014-04-05 13:23:18 +0200

    skeleton for our processes

---
+++ integration.js
@@ -0,0 +1,22 @@
+function reloadPlayer() {
+
+	// request mood from sentiment analysis
+	
+	/* // do a get request
+	$.ajax({
+	    url: 'http://url/path/',
+	    type: 'GET',
+	    data: 'param1=value1&param2=value',
+	    success: function() { alert('GET completed'); }
+	});
+	*/
+	
+	// map argus labs mood to gracenote mood
+	
+	// get gracenote tracks
+	
+	// play tracks with deezer player
+
+	alert("done");
+
+}
