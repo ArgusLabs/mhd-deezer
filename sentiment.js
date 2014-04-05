cd7da4be7c638d56c7f7588d7198b04ab8a63f4b
Ben Van Daele <me@benvd.be>
2014-04-05 22:33:13 +0200

    Send current mood along with mood polling

--- sentiment.js
+++ sentiment.js
@@ -1,8 +1,14 @@
 function getSentiment(callback) {
 	
+	var currentMoodParams;
+	if (currentMood) {
+		currentMoodParams = '?current_mood=' + currentMood;
+	} else {
+		currentMoodParams = '';
+	}
 	// do a get request
 	$.ajax({
-	    url: 'http://mhdapi-640468004.eu-west-1.elb.amazonaws.com/users/529c44edaae8a3071b000003/mood',
+	    url: 'http://mhdapi-640468004.eu-west-1.elb.amazonaws.com/users/529c44edaae8a3071b000003/mood' + currentMoodParams,
 	    type: 'GET',
 	    // data: 'param1=value1&param2=value',
 	    success: function(data) { 
