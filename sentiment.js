2a96aa5e10c4ca1dc371a6cbbaec8c6ce19278db
Roel Berger <berger.roel@gmail.com>
2014-04-05 14:17:13 +0200

    Added lookup to service for mood values

--- sentiment.js
+++ sentiment.js
@@ -1,13 +1,20 @@
-function getSentiment() {
+function getSentiment(callback) {
 	
-	/* // do a get request
+	// do a get request
 	$.ajax({
-	    url: 'http://url/path/',
+	    url: 'http://mhdapi-640468004.eu-west-1.elb.amazonaws.com/users/529c44edaae8a3071b000003/mood',
 	    type: 'GET',
-	    data: 'param1=value1&param2=value',
-	    success: function() { alert('GET completed'); }
+	    // data: 'param1=value1&param2=value',
+	    success: function(data) { 
+			if(data == undefined) {
+				console.log("Data is null!!");
+			}
+			console.log(data);
+			if(callback != undefined && typeof callback == 'function') {
+				callback(data);
+			}
+		}
 	});
-	*/
 	
 	return 1;
 }
