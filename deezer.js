74b951ae449571bf6777d5fe5accafe822987e61
Anthony Liekens <anthony@liekens.net>
2014-04-06 03:14:04 +0200

    good riddance, dear debugging purposes event_listeners

--- deezer.js
+++ deezer.js
@@ -19,27 +19,22 @@ $(document).ready(function(){
 // event listener, for now we display it for debugging purposes
 
 function event_listener_append() {
-	var pre = document.getElementById('event_listener');
+/*	var pre = document.getElementById('event_listener');
 	var line = [];
 	for (var i = 0; i < arguments.length; i++) {
 		line.push(arguments[i]);
 	}
 	pre.innerHTML += line.join(' ') + "\n";
+*/
 }
 var playerLoaded = false;
 function onPlayerLoaded() {
 	$("#controlers input").attr('disabled', false);
-	event_listener_append('player_loaded');
-	DZ.Event.subscribe('current_track', function(arg){
-		event_listener_append('current_track', arg.index, arg.track.title, arg.track.album.title);
-	});
 	DZ.Event.subscribe('player_position', function(arg){
-		event_listener_append('position', arg[0], arg[1]);
 		$("#slider_seek").find('.bar').css('width', (100*arg[0]/arg[1]) + '%');
 	});
 
 	DZ.Event.subscribe('track_end', function() {
-		event_listener_append('track_end');
 		playNextTrack();
 	});
 	playerLoaded = true;
