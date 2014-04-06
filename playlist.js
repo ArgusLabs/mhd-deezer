65ed4411ca0ddfae0683e591d6fe2be62928770b
Anthony Liekens <anthony@liekens.net>
2014-04-06 02:06:43 +0200

    style upcoming songs

--- playlist.js
+++ playlist.js
@@ -60,6 +60,6 @@ function renderPlaylist() {
 	playlistElement = $( "#playlist" );
 	playlistElement.empty();
 	for (var i = 0; i < playlist.length; i++) {
-		playlistElement.append('<li><div class="title">' + playlist[ i ][ "title" ] + '</div><div class="artist">' + playlist[ i ][ "artist" ][ "name" ] + '</div></li>');
+		playlistElement.append('<li><div class="trackcontainer"><div class="title" class="' + playlist[ i ][ "id" ] + '">' + playlist[ i ][ "title" ] + '</div><div class="artist">' + playlist[ i ][ "artist" ][ "name" ] + '</div></li>');
 	}
 }
